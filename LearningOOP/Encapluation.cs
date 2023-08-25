e5850a9606303c57ed10555486326b8b18b45197
Prashishgt <prashish24@gmail.com>
2023-08-25 19:40:14 +0545

    Learned new concept of encapsulation of OOP

---
+++ LearningOOP/Encapluation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningOOP
+{
+    internal class Person
+    {
+        // private key word denotes only member of same class can access it;
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private string _phone;
+        private string _address;
+
+        public Person() { }
+        public Person(int id, string firstName, string lastName, string email, string phone, string address)
+        {
+            _id = id;
+            _firstName = firstName;
+            _lastName = lastName;
+            _email = email;
+            _phone = phone;
+            _address = address;
+        }
+        public int Id
+        {
+            get { return _id; }
+            set { this._id = value; }
+        }
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { this._firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { this._lastName = value; }
+        }
+        public String Email
+        {
+            get { return _email; }
+            set { this._email = value; }
+        }
+        public string Phone
+        {
+            get { return _phone; }
+            set { this._phone = value; }
+        }
+        public string Address
+        {
+            get { return _address; }
+            set { this._address = value; }
+        }
+    }
+}
--- LearningOOP/Program.cs
+++ LearningOOP/Program.cs
@@ -10,11 +10,22 @@ namespace LearningOOP
     {
         static void Main(string[] args)
         {
+            // implementing abstraction 
             Circle circle = new Circle(5.0);
             Reactangle rectangle = new Reactangle(4.0, 6.0);
 
             circle.Display();     
             rectangle.Display();
+
+            // implementation of encapsulation
+            Person p1 = new Person(1, "Harry", "Styles", "harry@gmail.com", "9813246717", "London");
+
+            Console.WriteLine(p1.FirstName);
+            Console.WriteLine(p1.LastName);
+            Console.WriteLine(p1.Phone);
+            string fName = "Prashish";
+            p1.FirstName = fName;
+            Console.WriteLine($"This change is after changing \"{p1.FirstName}\"");
             Console.ReadLine();
         }
     }
