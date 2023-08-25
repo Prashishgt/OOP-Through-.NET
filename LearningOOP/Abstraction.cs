27f48997e10bc4cd1f033852ab78e085dd68f93e
Prashishgt <prashish24@gmail.com>
2023-08-25 18:09:08 +0545

    Implemented Abstraction

---
+++ LearningOOP/Abstraction.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningOOP
+{
+    internal abstract class Shape
+    {
+        public abstract double CalculateArea();
+
+        public void Display()
+        {
+            Console.WriteLine($"Area: {CalculateArea()}");
+        }
+    }
+
+    class Circle: Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    class Reactangle: Shape
+    {
+        private double length, breadth;
+
+        public Reactangle(double length, double breadth)
+        {
+            this.breadth = breadth;
+            this.length = length;
+        }
+
+        public override double CalculateArea()
+        {
+            return length * breadth;
+        }
+    }
+
+
+}
---
+++ LearningOOP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5.0);
+            Reactangle rectangle = new Reactangle(4.0, 6.0);
+
+            circle.Display();     
+            rectangle.Display();
+            Console.ReadLine();
+        }
+    }
+}
